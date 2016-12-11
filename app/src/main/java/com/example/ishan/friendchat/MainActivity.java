01b25382a596e4ae517bdc74cad281b5d4922ba5
ishan-maheshwari <ishoo1997@gmail.com>
2016-12-11 17:01:24 +0530

    added sign-in

--- app/src/main/java/com/example/ishan/friendchat/MainActivity.java
+++ app/src/main/java/com/example/ishan/friendchat/MainActivity.java
@@ -1,13 +1,65 @@
 package com.example.ishan.friendchat;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int SIGN_IN_REQUEST_CODE = 1 ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if(FirebaseAuth.getInstance().getCurrentUser() == null) {
+            //Start sign in/sign up activity
+            startActivityForResult(AuthUI.getInstance()
+                    .createSignInIntentBuilder()
+                    .build(),
+                    SIGN_IN_REQUEST_CODE
+            );
+        }
+        else {
+            Toast.makeText(this,
+                    "Welcome" + FirebaseAuth.getInstance()
+                        .getCurrentUser()
+                        .getDisplayName(),
+                    Toast.LENGTH_LONG)
+                    .show();
+            displayChatMessages();
+        }
+    }
+    
+    private void displayChatMessages(){
+        
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        
+        if(requestCode == SIGN_IN_REQUEST_CODE) {
+            if(resultCode == RESULT_OK) {
+                Toast.makeText(this,
+                        "Successfully signed in. Welcome!",
+                        Toast.LENGTH_LONG)
+                        .show();
+                displayChatMessages();
+            } else {
+                Toast.makeText(this,
+                        "We couldn't sign you in. Please try again later.",
+                        Toast.LENGTH_LONG)
+                        .show();
+                
+                finish();
+            }
+        }
     }
 }
+
