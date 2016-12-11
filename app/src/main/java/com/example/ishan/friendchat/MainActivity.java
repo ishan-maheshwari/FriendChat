4481bd8d4515b7e009f0385332871b7d4abd1b90
ishan-maheshwari <ishoo1997@gmail.com>
2016-12-11 19:02:36 +0530

    post chat on firebase database

--- app/src/main/java/com/example/ishan/friendchat/MainActivity.java
+++ app/src/main/java/com/example/ishan/friendchat/MainActivity.java
@@ -2,25 +2,31 @@ package com.example.ishan.friendchat;
 
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final int SIGN_IN_REQUEST_CODE = 1 ;
+    FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        fab = (FloatingActionButton)findViewById(R.id.fab);
 
         if(FirebaseAuth.getInstance().getCurrentUser() == null) {
             //Start sign in/sign up activity
@@ -39,6 +45,21 @@ public class MainActivity extends AppCompatActivity {
                     .show();
             displayChatMessages();
         }
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText input = (EditText)findViewById(R.id.input);
+
+                FirebaseDatabase.getInstance()
+                        .getReference()
+                        .push()
+                        .setValue(new ChatMessage(input.getText().toString(),
+                                FirebaseAuth.getInstance().getCurrentUser().getDisplayName()));
+
+                input.setText("");
+            }
+        });
     }
     
     private void displayChatMessages(){
